1dcca7e37d920ca5798bf29b82cd07e5ebf4aa5b
usarneme <jamestcoop@gmail.com>
2020-02-27 22:56:32 -0800

    Added vehicle lookup functionality to the Accounts route/page. API is queried and updates selects after having chosen a year and make to populate which models are available as an html select option. Updated some variable/div names to be more descriptive. Updated the front end JS which pulls data from the vehicle information database via axios/ajax.

--- public/javascripts/modules/vehicleSearch.js
+++ public/javascripts/modules/vehicleSearch.js
@@ -10,10 +10,19 @@ function listeners() {
   console.log('Setting up event listeners for vehicle searching...')
   const vinApproach = document.querySelector('#searchByVin')
   const manualApproach = document.querySelector('#manualVehicleEntry')
+  const lookupApproach = document.querySelector('#vehicleLookup')
+
   const vinSearchButton = document.querySelector('#vinSearchButton')
+  const makeLookupSelector = document.querySelector('select[name="lookupMake"]')
+  const modelLookupSelector = document.querySelector('select[name="lookupModel"]')
+
   vinApproach.on('click', e => changeSearchView(e, 'vin'))
   manualApproach.on('click', e => changeSearchView(e, 'manual'))
+  lookupApproach.on('click', e => changeSearchView(e, 'lookup'))
+
   vinSearchButton.on('click', e => vinSearch(e))
+  makeLookupSelector.on('change', e => lookupMakeQuery(e))
+  modelLookupSelector.on('change', e => modelSelected(e))
 }
 
 function changeSearchView(e, approach = 'manual') {
@@ -24,21 +33,34 @@ function changeSearchView(e, approach = 'manual') {
   e.preventDefault()
   const vinDiv = document.querySelector('.searchByVinDiv')
   const manualDiv = document.querySelector('.manualVehicleEntryDiv')
+  const lookupDiv = document.querySelector('.vehicleLookupDiv')
+
   const vinSwitcherButton = document.querySelector('.vinLookupButton')
   const manualSwitcherButton = document.querySelector('.manualVehicleEntryButton')
+  const lookupSwitcherButton = document.querySelector('.vehicleLookupButton')
 
   if (approach === 'vin') {
     vinDiv.classList.remove('hidden')
-    manualDiv.classList.add('hidden')
     vinSwitcherButton.classList.add('selected')
+    manualDiv.classList.add('hidden')
     manualSwitcherButton.classList.remove('selected')
-
-  } else {
+    lookupDiv.classList.add('hidden')
+    lookupSwitcherButton.classList.remove('selected')
+  } else if (approach === 'manual') {
     manualDiv.classList.remove('hidden')
+    manualSwitcherButton.classList.add('selected')
     vinDiv.classList.add('hidden')
     vinSwitcherButton.classList.remove('selected')
-    manualSwitcherButton.classList.add('selected')
-  } 
+    lookupDiv.classList.add('hidden')
+    lookupSwitcherButton.classList.remove('selected')
+  } else if (approach === 'lookup') {
+    lookupDiv.classList.remove('hidden')
+    lookupSwitcherButton.classList.add('selected')
+    manualDiv.classList.add('hidden')
+    manualSwitcherButton.classList.remove('selected')
+    vinDiv.classList.add('hidden')
+    vinSwitcherButton.classList.remove('selected')
+  }
 }
 
 function vinSearch(e) {
@@ -65,20 +87,80 @@ function vinSearch(e) {
         const rawMake = results[6]["Value"]
         const make = rawMake.charAt(0).toUpperCase() + rawMake.slice(1).toLowerCase()
         const model = results[8]["Value"]
-        const updateButton = document.querySelector('#confirmResults')
+        const confirmVinResultsButton = document.querySelector('#confirmVinResults')
 
         resultsDiv.innerHTML = `
           <div class="vinSearchResultsHeader">VIN Search Results:</div>
           <div class="vinSearchResult">
             <div>Year: ${year}</div><div>Make: ${make}</div><div>Model: ${model}</div>
           </div>`
-        updateButton.on('click', e => updateVehicle(e, year, make, model))
-        updateButton.classList.remove('hidden')
+        confirmVinResultsButton.on('click', e => updateVehicle(e, year, make, model))
+        confirmVinResultsButton.classList.remove('hidden')
       } else {
         resultsDiv.textContent = `No Results Found for VIN "${vin}". Please try again.`
       }
     })
     .catch(err => console.error(err))
+}
+
+function lookupMakeQuery(e) {
+  e.stopPropagation()
+  e.preventDefault()
+  const make = e.originalTarget.value
+  if (!make) return
+  console.log("Select changed. Calling lookupMakeQuery... "+make)
+
+  const year = document.querySelector('select[name="lookupYear"]').value
+  const modelLookupSelect = document.querySelector('select[name="lookupModel"]')
+  while (modelLookupSelect.firstChild) {
+    modelLookupSelect.removeChild(modelLookupSelect.lastChild)
+  }
+
+  const loadingOption = document.createElement('option')
+  loadingOption.value = 'Loading data...'
+  loadingOption.innerHTML = 'Loading data...'
+  modelLookupSelect.appendChild(loadingOption)
+
+  axios
+    .get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${make}/modelyear/${year}?format=json`)
+    .then(data => {
+      while (modelLookupSelect.firstChild) {
+        modelLookupSelect.removeChild(modelLookupSelect.lastChild)
+      }
+      let models = data.data["Results"]
+      models.sort()
+      // console.dir(models)
+
+      const emptyOption = document.createElement('option')
+      emptyOption.disabled = 'disabled'
+      emptyOption.selected = 'selected'
+      emptyOption.value = 'Select a model...'
+      emptyOption.innerHTML = 'Select a model...'
+      modelLookupSelect.appendChild(emptyOption)
+
+      models.forEach(result => {
+        // console.log('Setting up select option for '+result["Model_Name"])
+        const opt = document.createElement('option')
+        opt.value = result["Model_Name"]
+        opt.innerHTML = result["Model_Name"]
+        modelLookupSelect.appendChild(opt)
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+function modelSelected(e) {
+  e.stopPropagation()
+  e.preventDefault()
+  console.log('Model selected...')
+  const year = document.querySelector('select[name="lookupYear"]').value
+  const make = document.querySelector('select[name="lookupMake"]').value
+  const model = e.originalTarget.value
+  console.log(year, make, model)
+
+  const confirmLookupResultsButton = document.querySelector('#confirmLookupResults')
+  confirmLookupResultsButton.on('click', e => updateVehicle(e, year, make, model))
+  confirmLookupResultsButton.classList.remove('hidden')
 }
 
 function updateVehicle(e, year, make, model) {
