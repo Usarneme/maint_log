4a0d06085f39e34047ba91d158199aa54a743cbc
usarneme <jamestcoop@gmail.com>
2020-03-04 02:41:14 -0800

    Added testing for get routes.

--- tests/__tests__/server.spec.js
+++ tests/__tests__/server.spec.js
@@ -11,7 +11,7 @@ const app = require('../../app')
 app.set('port', process.env.PORT || 7777)
 const request = supertest(app)
 
-describe('Loading Express Server', () => {
+describe('Testing Routes', () => {
 
   it('GET / route returns 200', async done => {
     const response = await request.get('/')
@@ -21,4 +21,60 @@ describe('Loading Express Server', () => {
     done()
   })
 
+  it('GET /log route returns 302 as user is not logged in', async done => {
+    const response = await request.get('/log')
+    expect(response.redirect).toBe(true)
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(302)
+    done()
+  })
+
+  it('GET /add route returns 302 as user is not logged in', async done => {
+    const response = await request.get('/add')
+    expect(response.redirect).toBe(true)
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(302)
+    done()
+  })
+
+  it('GET /upcoming-maintenance route returns 302 as user is not logged in', async done => {
+    const response = await request.get('/upcoming-maintenance')
+    expect(response.redirect).toBe(true)
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(302)
+    done()
+  })
+
+  it('GET /account route returns 302 as user is not logged in', async done => {
+    const response = await request.get('/account')
+    expect(response.redirect).toBe(true)
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(302)
+    done()
+  })
+
+  it('GET /login route returns 200', async done => {
+    const response = await request.get('/login')
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(200)
+    done()
+  })
+
+  it('GET /register route returns 200', async done => {
+    const response = await request.get('/register')
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(200)
+    done()
+  })
+
 })
+
+// get('/log/page/:page
+// post('/add',
+// post('/add/:id',
+// get('/log/:id/edit
+// get('/log/:slug',
+// get('/login'
+// post('/register'
+// get('/logout'
+// post('/account'
