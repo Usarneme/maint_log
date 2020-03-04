902f95f11f556783f64ca5bdcfd8de0e9dc50f0e
usarneme <jamestcoop@gmail.com>
2020-03-03 21:08:22 -0800

    Completed Jest testing suite setup. Created a setupTests.js helper for creating a local db for testing functionality coverage.

--- tests/__tests__/server.spec.js
+++ tests/__tests__/server.spec.js
@@ -1,21 +1,23 @@
 const supertest = require('supertest')
 
-require('../models/Log')
-require('../models/User')
-require('../models/Vehicle')
+require('../../models/Log')
+require('../../models/User')
+require('../../models/Vehicle')
 
-const { setupDB } = require('../test-setup')
+const { setupDB } = require('../setupTests')
 setupDB('endpoint-testing')
 
-const app = require('../app')
+const app = require('../../app')
 app.set('port', process.env.PORT || 7777)
 const request = supertest(app)
 
 describe('Loading Express Server', () => {
 
-  it('responds to /', async done => {
+  it('GET / route returns 200', async done => {
     const response = await request.get('/')
     expect(response).toEqual(expect.anything())
+    expect(response.req.method).toEqual('GET')
+    expect(response.res.statusCode).toEqual(200)
     done()
   })
 
