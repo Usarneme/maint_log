8a73f48723ca5073996d6c71888db7186f4803bb
usarneme <jamestcoop@gmail.com>
2020-03-11 16:58:09 -0700

    Added CORS package to app.js to allow resource sharing to the React front end that is hosted elsewhere (currently localhost, but some other hosting provider when ready for production).

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const { promisify } = require('es6-promisify')
 const flash = require('connect-flash')
+const cors = require('cors')
 
 const routes = require('./routes/index')
 const helpers = require('./helpers')
@@ -60,6 +61,14 @@ app.use((req, res, next) => {
   req.login = promisify(req.login, req)
   next()
 })
+
+app.use(cors())
+// Non package CORS implementation
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD") // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 
 app.use('/', routes)
 
