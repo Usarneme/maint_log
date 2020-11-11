1c8e95c1648a85805e04265793970a5b86cc1100
usarneme <jamestcoop@gmail.com>
2020-11-10 21:45:02 -0800

    ran npm audit fix in client/ and main project folders to update dependencies.

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.833b1683bdc7dce8cda26c7ef036a30f.js"
+  "/precache-manifest.df71924eb1ed7f3bcae7d686d093ff63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
